541e93d7ccbcab367752b63838cee406326b63a7
Kainar <toleubaevkai@mail.ru>
2021-03-12 17:52:40 +0600

    style-component

--- src/components/itemList/itemList.js
+++ src/components/itemList/itemList.js
@@ -1,20 +1,22 @@
 import React, {Component} from 'react';
-import './itemList.css';
+import {ListGroupItem, ListGroup} from '../randomChar/randomChar';
+
+
 export default class ItemList extends Component {
 
     render() {
         return (
-            <ul className="item-list list-group">
-                <li className="list-group-item">
+            <ListGroup>
+                <ListGroupItem Cursor>
                     John Snow
-                </li>
-                <li className="list-group-item">
+                </ListGroupItem>
+                <ListGroupItem Cursor>
                     Brandon Stark
-                </li>
-                <li className="list-group-item">
+                </ListGroupItem>
+                <ListGroupItem Cursor>
                     Geremy
-                </li>
-            </ul>
+                </ListGroupItem>
+            </ListGroup>
         );
     }
 }
--- src/components/randomChar/randomChar.js
+++ src/components/randomChar/randomChar.js
@@ -1,32 +1,77 @@
 import React, {Component} from 'react';
-import './randomChar.css';
+import styled, {css} from 'styled-components';
+
+const RandomBlock = styled.div`
+        background-color: #fff;
+        padding: 25px 25px 15px 25px;
+        margin-bottom: 40px;
+        border-radius: 0.25rem !important;
+        h4 {
+            margin-bottom: 20px;
+            text-align: center;
+        }
+`;
+
+const Term = styled.span`
+    font-weight: bold;
+`;
+
+const ListGroupItem = styled.li`
+        position: relative;
+        display: flex;
+        justify-content: space-between;
+        padding: 0.75rem 1.25rem;
+        background-color: #fff;
+        border: 1px solid rgba(0, 0, 0, 0.125);
+        border-right: 0;
+        border-left: 0;
+        border-top: 0;
+        border-radius: 0;
+        ${props => props.Cursor && css`
+            cursor:pointer;
+        `}
+`;
+
+const ListGroup = styled.ul`
+        display: flex;
+        -ms-flex-direction: column;
+        flex-direction: column;
+        padding-left: 0;
+        margin-bottom: 0;
+        border-radius: 0.25rem;
+        li {
+            border-radius: 0.25rem;
+        }
+`;
 
 export default class RandomChar extends Component {
 
     render() {
 
         return (
-            <div className="random-block rounded">
+            <RandomBlock>
                 <h4>Random Character: John</h4>
-                <ul className="list-group list-group-flush">
-                    <li className="list-group-item d-flex justify-content-between">
-                        <span className="term">Gender </span>
+                <ListGroup>
+                    <ListGroupItem>
+                        <Term>Gender </Term>
                         <span>male</span>
-                    </li>
-                    <li className="list-group-item d-flex justify-content-between">
-                        <span className="term">Born </span>
+                    </ListGroupItem>
+                    <ListGroupItem>
+                        <Term>Born </Term>
                         <span>11.03.1039</span>
-                    </li>
-                    <li className="list-group-item d-flex justify-content-between">
-                        <span className="term">Died </span>
+                    </ListGroupItem>
+                    <ListGroupItem>
+                        <Term>Died </Term>
                         <span>13.09.1089</span>
-                    </li>
-                    <li className="list-group-item d-flex justify-content-between">
-                        <span className="term">Culture </span>
+                    </ListGroupItem>
+                    <ListGroupItem>
+                        <Term>Culture </Term>
                         <span>Anarchy</span>
-                    </li>
-                </ul>
-            </div>
+                    </ListGroupItem>
+                </ListGroup>
+            </RandomBlock>
         );
     }
 }
+
+export {ListGroupItem, ListGroup, Term};
